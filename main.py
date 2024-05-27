112822618663c958d97f3ba14777d6b4786018a6
Maxim2802 <maximtchebykin@yandex.ru>
2024-05-28 01:13:00 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+#  1 Задание
+a = "Хочу дачу на Кубе"
+print(len(a))
+
+#  2 Задание
+first = 5
+second = 8
+summa = (first + second)
+diff = (first - second)
+print(summa, diff)
+
+#  3 Задание
+first = 21
+second = 7
+third = 3
+mean = ((first + second + third) / 3)
+print(mean)
+
+#  4 Задание
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ', ' + first_string)
+
+#  5 Задание
+a = 8
+b = 2
+c = 3
+f = ((a * b) + (a * c))
+print(f ** 3 // 2)
